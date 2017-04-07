0e8799fcf41cb56158520b87357cee05f6274c8a
nzsquirrell <squirrell@nutty.one>
2017-04-08 08:53:08 +1200

    Skein cleanups

--- algo/skein.c
+++ algo/skein.c
@@ -30,8 +30,6 @@ int scanhash_skein(int thr_id, uint32_t *pdata, const uint32_t *ptarget, uint32_
 {
 	uint32_t _ALIGN(128) hash32[8];
 	uint32_t _ALIGN(128) endiandata[20];
-	// uint32_t *pdata = work->data;
-	// uint32_t *ptarget = work->target;
 
 	const uint32_t Htarg = ptarget[7];
 	const uint32_t first_nonce = pdata[19];
@@ -46,7 +44,6 @@ int scanhash_skein(int thr_id, uint32_t *pdata, const uint32_t *ptarget, uint32_
 		be32enc(&endiandata[19], n);
 		skeinhash(hash32, endiandata);
 		if (hash32[7] < Htarg && fulltest(hash32, ptarget)) {
-			//work_set_target_ratio(work, hash32);
 			*hashes_done = n - first_nonce + 1;
 			pdata[19] = n;
 			return true;
